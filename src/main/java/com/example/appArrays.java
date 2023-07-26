1c50e6da8a86f719783fab3e902c70a16b044279
ELISERPRO SILVA <elianems23@gmail.com>
2023-07-26 20:21:10 +0000

    testevetor.java

--- src/main/java/com/example/appArrays.java
+++ src/main/java/com/example/appArrays.java
@@ -1,8 +1,10 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class appArrays {
     public static void main(String[] args) {
         double vetor [] = {10,20,30};
-        System.out.println(vetor);
+        System.out.println(Arrays.toString(vetor));
     }
 }
